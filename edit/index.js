4ec32312ece887a48e3704c6cd1695556388250a
Allan Dollé <dolle.allan@gmail.com>
2017-12-03 05:11:36 +0100

    Add autoType animation

--- edit/index.js
+++ edit/index.js
@@ -5,6 +5,25 @@ var submit = document.getElementById('submit');
 input.classList.add("show");
 // input.focus();
 
+// Typing text function
+function autoType(element, typingSpeed, delay) {
+  var selector = element;
+  var text = element.value.trim().split('');
+  var amntOfChars = text.length;
+  var newString = "";
+  setTimeout(function() {
+    selector.value = '';
+    for (var i = 0; i < amntOfChars; i++) {
+      (function(i, char) {
+        setTimeout(function() {
+          newString += char;
+          selector.value = newString;
+        }, i * typingSpeed);
+      })(i + 1, text[i]);
+    }
+  }, delay);
+}
+
 input.onfocus = function() {
   var placeholder = input.getAttribute("placeholder");
   input.setAttribute("placeholder", '');
@@ -12,13 +31,13 @@ input.onfocus = function() {
 };
 
 function writePlaceHolder(placeholder) {
-  console.log(placeholder);
   input.setAttribute("placeholder", placeholder);
 }
 
 function changeHTML(array) {
   input.setAttribute("placeholder", array[0]);
   submit.value = array[1];
+  autoType(submit, 20, 0);
   if (array[2]) {
     input.classList.add("show");
     // input.style.display = 'block';
